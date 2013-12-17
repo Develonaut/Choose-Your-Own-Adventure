2dbb418b3d1d27f9a87cdd894d2b76735c3fc448
Develonaut <ryanmmchenry@gmail.com>
2013-12-17 11:07:49 -0500

    Create .js

---
+++ .js
@@ -0,0 +1,26 @@
+/* Author: Created By Ryan McHenry, ryanmmchenry@gmail.com,
+Visit my personal site: www.develonaut.com */ 
+confirm("Time to go on an adventure!") 
+var age = prompt("What is your age?"); 
+if (age >= 18) { 
+    confirm("Welcome to my game!"); 
+} 
+else { 
+    confirm("You are allowed to play but I take no responsibility."); 
+} 
+confirm("Snow White and Batman were hanging out at the bus stop, waiting to go to the shops. There was a sale on and both needed some new threads. You've never really liked Batman. You walk up to him."); 
+confirm("Batman glares at you."); 
+var userAnswer = prompt("Are you feeling lucky punk? Yes or No."); 
+if (userAnswer === "yes") { 
+    confirm("Batman hits you very hard. It's Batman and you're you, of course Batman wins!") 
+} 
+else { 
+    confirm("Good choice! No one beats the Batman.") 
+} 
+var feedback = prompt("Would you kindly rate my game out of 10?"); 
+if (feedback > 8) { 
+    confirm("This is just the beginning of my game empire. Stay tuned for more!") 
+} 
+else { 
+    confirm("I slaved away at this game and you gave me that score?! The nerve! Just you wait!") 
+} 
